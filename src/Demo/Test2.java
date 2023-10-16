0360c7d99072f88d6f2a1e5708fc03e66d41f7cc
User <User@DESKTOP-RN0EGD7>
2023-10-16 10:55:06 +0530

    Commit test2 to preranaBranch

---
+++ src/Demo/Test2.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+     System.out.println("Rahul Horkate Jindabad");
+     
+     System.out.println("Prerana Pisal Jindabad");
+     
+     System.out.println("Prerana Kamble Jindabad");
+
+
+	}
+
+}
